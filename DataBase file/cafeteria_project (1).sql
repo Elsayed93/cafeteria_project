77fa22564f21b9fdc512ad6500dc5343e1eac100
Elsayed <elsayedelbeshry119@gmail.com>
2020-11-20 00:24:07 +0200

    add category table in data base

--- DataBase file/cafeteria_project (1).sql
+++ DataBase file/cafeteria_project (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2020 at 08:24 PM
+-- Generation Time: Nov 19, 2020 at 11:22 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -24,16 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cate_id` int(11) NOT NULL,
+  `category` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `order_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `status` varchar(25) NOT NULL,
+  `status` varchar(25) DEFAULT NULL,
   `amount` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `notes` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_date`, `status`, `amount`, `user_id`, `notes`) VALUES
+(1, '2020-11-16 10:54:18', NULL, 20, 3, NULL),
+(2, '2020-11-16 13:37:50', NULL, 25, 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -43,8 +63,20 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `orders_products` (
   `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(25) NOT NULL,
+  `product_quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders_products`
+--
+
+INSERT INTO `orders_products` (`order_id`, `product_id`, `product_name`, `product_quantity`) VALUES
+(1, 1, 'tea', 2),
+(1, 2, 'cofee', 1),
+(2, 1, 'tea', 1),
+(2, 2, 'cofee', 2);
 
 -- --------------------------------------------------------
 
@@ -54,10 +86,10 @@ CREATE TABLE `orders_products` (
 
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL,
-  `product_name` varchar(50) NOT NULL,
+  `product_name` varchar(25) NOT NULL,
   `price` varchar(20) NOT NULL,
   `category` varchar(30) NOT NULL,
-  `product_profile` varchar(50) DEFAULT NULL
+  `product_profile` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -65,9 +97,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `price`, `category`, `product_profile`) VALUES
-(1, 'tea', '5 LE', 'Hot Drinks', NULL),
-(2, 'cofee', '10 LE', 'Hot Drinks', NULL),
-(3, 'cola', '5 LE', 'cold Drinks', NULL);
+(1, 'tea', '5 LE', 'Hot Drinks', 'tea.png'),
+(2, 'cofee', '10 LE', 'Hot Drinks', 'cofee.png'),
+(3, 'cola', '5 LE', 'cold Drinks', 'cola.png');
 
 -- --------------------------------------------------------
 
@@ -91,15 +123,21 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `room_number`, `EXT`, `profile_picture`, `is_admin`) VALUES
-(1, 'omar pero', 'omar@cafee.com', 'Omar123@#', 112, NULL, NULL, 0),
-(2, 'sherief', 'sherief@cafee.com', 'Sherief123@#', 113, NULL, NULL, 0),
-(3, 'Elsayed Elbeshry', 'elsayed@cafee.com', 'Elsayed123@#', 113, NULL, NULL, 0),
-(4, 'Reem Mousa', 'reem@cafee.com', 'Reem123@#', 114, NULL, NULL, 0),
-(5, 'Elene Malak', 'elene@cafee.com', 'Elene123@#', 112, NULL, NULL, 0);
+(1, 'omar pero', 'omar@cafee.com', 'Omar123@#', 112, NULL, 'user.png', 0),
+(2, 'sherief', 'sherief@cafee.com', 'Sherief123@#', 113, NULL, 'user.png', 0),
+(3, 'Elsayed Elbeshry', 'elsayed@cafee.com', 'Elsayed123@#', 113, NULL, 'admin.png', 1),
+(4, 'Reem Mousa', 'reem@cafee.com', 'Reem123@#', 114, NULL, 'user.png', 0),
+(5, 'Elene Malak', 'elene@cafee.com', 'Elene123@#', 112, NULL, 'user.png', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cate_id`);
 
 --
 -- Indexes for table `orders`
@@ -112,6 +150,7 @@ ALTER TABLE `orders`
 -- Indexes for table `orders_products`
 --
 ALTER TABLE `orders_products`
+  ADD PRIMARY KEY (`order_id`,`product_id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
 
@@ -132,10 +171,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cate_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
